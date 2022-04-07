2c15c6489da2d125b4404f2c32eb9d019c33784f
4thDec <garibeshubangi@gmail.com>
2022-04-07 23:40:14 +0530

    2nd change

--- src/test/java/simple/welcome.java
+++ src/test/java/simple/welcome.java
@@ -5,6 +5,7 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("welcome");
+         System.out.println("Welcome to my class");
 	}
 
 }
